e68e5287964ab5a7b264093bce2937392d6bdb56
Molly O'Neil <mbethoneil@gmail.com>
2020-08-08 19:29:58 -0400

    data_import method, the web scraping part

---
+++ web_data.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
+import time
+
+def data_import():
+    titles = []
+    i = 19
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    page = "https://podcasts.apple.com/us/podcast/small-town-murder/id1194755213"
+    driver.get(page)
+    while i > 0:
+        # Wait for the next "Load more episodes" button to appear
+        # Also don't want to get kicked off the site for traffic issues...
+        time.sleep(4)
+        # There are three buttons found with the following css selector.
+        # Click the one in the middle, because that is the "Load more episodes" button
+        (driver.find_elements_by_css_selector('button.link'))[1].click()
+        i = i - 1
+    content = driver.page_source
+    data = BeautifulSoup(content, features="html.parser")
+    for episode in data.findAll('div', attrs={'class': 'tracks__track__content'}):
+        title = episode.find('a', attrs={'class': 'tracks__track__link--block'}).text.strip()
+        titles.append(title)
+    driver.close()
+    return titles
