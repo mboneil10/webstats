fd69b9a5e1e684876eddb752949b435adf4dea75
Molly O'Neil <mbethoneil@gmail.com>
2020-08-09 12:35:57 -0400

    env set to miniconda

--- web_data.py
+++ web_data.py
@@ -5,6 +5,8 @@ import time
 import pandas as pd
 import csv
 import re
+import plotly.figure_factory as ff
+import numpy as np
 
 # TODO: gather the last ten titles
 # Only use this to reload titles.csv
@@ -71,4 +73,28 @@ def ranking(list):
             loc_count[loc] = 1
     return loc_count
 
-print(ranking(states(locations(read_titles_from_csv()))))
+df_sample = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/laucnty16.csv')
+df_sample['State FIPS Code'] = df_sample['State FIPS Code'].apply(lambda x: str(x).zfill(2))
+df_sample['County FIPS Code'] = df_sample['County FIPS Code'].apply(lambda x: str(x).zfill(3))
+df_sample['FIPS'] = df_sample['State FIPS Code'] + df_sample['County FIPS Code']
+
+colorscale = ["#f7fbff","#ebf3fb","#deebf7","#d2e3f3","#c6dbef","#b3d2e9","#9ecae1",
+              "#85bcdb","#6baed6","#57a0ce","#4292c6","#3082be","#2171b5","#1361a9",
+              "#08519c","#0b4083","#08306b"]
+endpts = list(np.linspace(1, 12, len(colorscale) - 1))
+fips = df_sample['FIPS'].tolist()
+values = df_sample['Unemployment Rate (%)'].tolist()
+
+fig = ff.create_choropleth(
+    fips=fips, values=values,
+    binning_endpoints=endpts,
+    colorscale=colorscale,
+    show_state_data=False,
+    show_hover=True, centroid_marker={'opacity': 0},
+    asp=2.9, title='USA by Unemployment %',
+    legend_title='% unemployed'
+)
+
+fig.layout.template = None
+fig.show()
+# ranking(states(locations(read_titles_from_csv())))
